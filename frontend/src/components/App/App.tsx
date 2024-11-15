5a1a057b0a2fe1a910f808c213dc3dc4714cb552
Marion Leblanc <marion.leblanc77@gmail.com>
2024-11-15 16:37:00 -0600

    Clean open and close feature

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 import { useAppDispatch, useAppSelector } from '../../store/hooks-redux';
@@ -15,17 +15,28 @@ function App() {
   // For managing error messages
   const errorMsg = useAppSelector((state) => state.genesetsReducer.errorMsg);
 
+  // const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
   // When the component does its first render, dispatches the action that retrieves and stores all genesets
   useEffect(() => {
     dispatch(getGenesets());
   }, []);
 
+  // const handleQuestionClick = (index: number) => {
+  //   setActiveIndex(activeIndex === index ? null : index);
+  // };
+
   return (
     <div className="App">
       {errorMsg && <p> {errorMsg} </p>}
-      <ul>
+      <ul className='App__genesetcatalog'>
         {genesetsCatalog.map((geneset)=>(
-          <Geneset key={geneset.id} geneset={geneset}/>
+          <Geneset 
+            key={geneset.id} 
+            geneset={geneset}           
+            // isOpen={activeIndex === geneset.id}
+            // onClick={() => handleQuestionClick(geneset.id)}
+          />
         ))}
       </ul>
     </div>
--- frontend/src/components/Geneset/Geneset.tsx
+++ frontend/src/components/Geneset/Geneset.tsx
@@ -1,19 +1,35 @@
+import { useRef, useState } from 'react';
 import { IGeneset } from '../../@types/genesets';
 import './Geneset.css';
+import Gene from './Gene/Gene';
 
 interface GenesetProps {
     geneset: IGeneset;
   }
 
-function Geneset({ geneset}: GenesetProps) {
+function Geneset({geneset}: GenesetProps) {
+  
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+
   return (
     <li className="Geneset">
-      <h1> {geneset.title} </h1>
-      <ul>
-        {geneset.genes.map((gene)=>(
-          <li>{gene.name}</li>
-        ))}
-      </ul>
+      <h1 onClick={onClick}> 
+        {isOpen ? "- " : "+ "}
+        {geneset.title} 
+      </h1>
+      {isOpen && 
+        <ul>
+          {geneset.genes.map((gene)=>(
+            <Gene key={gene.id} geneName={gene.name}/>
+          ))}
+        </ul>
+      }
     </li>
   );
 }
